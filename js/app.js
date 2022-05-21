68be9792b1c40654853cd6d048f8b940d3e9e5b2
fatkhullayevS <zfathullayev@gmail.com>
2022-05-21 10:20:52 +0500

    init: Example the end

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const travel = Number(prompt("summangizni kiriting"))
+const summa = (8316150 + 1397113);
+
+if ( travel >= summa){
+    console.log(`alisher oq yo'l sizning summangiz yetadi :)`);
+} else if(travel <= summa) {
+    console.log("Alisher biroz kutin sizning summangiz yetmas ekan :(");
+}
